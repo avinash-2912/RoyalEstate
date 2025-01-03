53af4575b306512078d7361d99c0910de41da16b
Avinash Kumar <ak7797405@gmail.com>
2025-01-03 23:05:23 +0530

    updated authjs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -69,6 +69,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: true,
+        sameSite: "none",
         maxAge: age,
       })
       .status(200)
