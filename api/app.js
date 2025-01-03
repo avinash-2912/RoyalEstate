f68adf63f229f40583c3b31934f8f6446c00420a
Avinash Kumar <ak7797405@gmail.com>
2025-01-03 23:40:04 +0530

    updated app.js

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ import messageRoute from "./routes/message.route.js";
 const app = express();
 
 app.set("trust proxy", 1); // Trust the proxy
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  console.log(token);
+  console.log("cookies",req.cookies)
+  console.log("token",token);
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
