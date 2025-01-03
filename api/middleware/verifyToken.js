6a09c13c6e3747b0e6130973199a1bcf794d631d
Avinash Kumar <ak7797405@gmail.com>
2025-01-03 23:58:16 +0530

    update middleware

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -2,9 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  console.log("cookies",req.cookies)
-  console.log("token",token);
-
+  
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
