1361d5eed67ae19f4c068103f71a7508dde05a86
Avinash Kumar <ak7797405@gmail.com>
2025-01-03 23:20:18 +0530

    updates app.js

--- api/app.js
+++ api/app.js
@@ -10,6 +10,7 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
+app.set("trust proxy", 1); // Trust the proxy
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
